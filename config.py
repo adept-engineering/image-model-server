036be0aecc28851c8bdd600e440c4cbd3e0cef8f
Fortune Oluwasemilore Alebiosu <fortune0208@yahoo.com>
2025-06-02 04:08:03 -0400

    changed model

--- config.py
+++ config.py
@@ -1,25 +1,27 @@
 import torch
 import base64
-from diffusers import FluxPipeline
+from diffusers import FluxPipeline, StableDiffusionPipeline, EulerDiscreteScheduler
 from io import BytesIO
 
+'''
 MODEL_NAME = "black-forest-labs/FLUX.1-dev"
 pipe = FluxPipeline.from_pretrained(MODEL_NAME, torch_dtype=torch.bfloat16)
+'''
+
+model_id = "stabilityai/stable-diffusion-2-1-base"
+
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
+
+pipe.enable_attention_slicing()
 
 #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
 #pipe.enable_model_cpu_offload() 
 
 def generate_image(text_prompt: str) -> tuple[bytes, str]:
 
-    image = pipe(
-        prompt=text_prompt,
-        height=1024,
-        width=1024,
-        guidance_scale=3.5,
-        num_inference_steps=50,
-        max_sequence_length=512,
-        generator=torch.Generator("cpu").manual_seed(0)
-    ).images[0]
+    image = pipe(prompt=text_prompt).images[0]  
 
     # Get image bytes
     img_byte_arr = BytesIO()
